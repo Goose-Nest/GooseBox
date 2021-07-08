14f53a3b5ecbc96b37cfeb00f6ed71e99a8534b6
Oj <oj@oojmed.com>
2021-07-08 14:10:53 +0100

    [Dynamic] Finish off rewrite

--- src/box.js
+++ src/box.js
@@ -1,6 +1,6 @@
 let patcherCore = ['patch', 'inject', 'uninject'];
 let allAPIs = [
-  'Commands', 'Webpack', 'Patcher', 'Settings', 'Toast', 'ConfirmDialog', // Core GM APIs
+  'Webpack', 'Patcher', 'Settings', 'Toast', 'ConfirmDialog', // Core GM APIs
   ...Object.keys(goosemod.patcher).filter((x) => !patcherCore.includes(x)).map((x) => x[0].toUpperCase() + x.substring(1)), // Standard patcher APIs
   'console'
 ];
@@ -191,18 +191,6 @@ let box = (jsCode, perms, meta = { name: 'Unknown' }) => {
       }
     }
   };
-
-  if (perms.includes('settings')) context.Settings = goosemod.settings;
-  if (perms.includes('toast')) context.Toast = goosemod.showToast;
-  if (perms.includes('confirm-dialog')) context.ConfirmDialog = goosemod.confirmDialog;
-
-  for (const key of Object.keys(goosemod.patcher).filter((x) => !patcherCore.includes(x))) {
-    const permKey = key.replace(/[a-z][A-Z]/g, (_) => `${_[0]}-${_[1].toLowerCase()}`); // contextMenu -> context-menu, username -> username
-    if (!perms.includes(permKey)) continue;
-
-    const globalKey = key[0].toUpperCase() + key.substring(1); // eg: contextMenu -> ContextMenu, username -> Username
-    context[globalKey] = goosemod.patcher[key];
-  }
 
   context.GooseBox = true;
   context.eval = window.eval;
